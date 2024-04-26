3cf5c21bb6c6782c1c715a9e51511c52a2e386b4
Abdulkadir KOCER <akocerweb@hotmail.com>
2024-04-26 15:47:52 +0300

    Delete uyg04-Work.UI directory

--- uyg04-Work.UI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace uyg04_Work.UI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
