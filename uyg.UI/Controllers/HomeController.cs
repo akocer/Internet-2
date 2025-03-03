b5a4350ee96b7581162350db5b95b4aed94db282
akocer <akocer@AKOCERHP>
2025-03-03 14:46:34 +0300

    03.03.2025

---
+++ uyg.UI/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace uyg.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Categories")]
+        public IActionResult Categories()
+        {
+            return View();
+        }
+
+        [Route("Products")]
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}
